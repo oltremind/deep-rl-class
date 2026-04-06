91141b2c6a274de85d8c4c2fa687c33420fb6680
Jason <nguyentronghieu.me@gmail.com>
2026-04-06 16:05:16 +0900

    Add baselines

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,14 @@
 # !/bin/bash
 
 apt update
-apt install swig -y
+apt install swig -y cmake
+apt install cmake
+
+apt-get update
+apt-get install -y python3-opengl
+install ffmpeg
+install xvfb
+install pyvirtualdisplay
+
 python3.10 -m pip install -r requirements.txt
+python3.10 -m pip install pyvirtualdisplay
